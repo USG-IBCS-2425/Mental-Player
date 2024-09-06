d3c56baea029e1f55db196f55fa0865f610d0c32
Lisha (Lisa) Liu <lisha_liu@m018761.aw.org>
2024-09-06 13:49:24 -0700

    message

---
+++ notes9_6.java
@@ -0,0 +1,39 @@
+class notes9_6 { //braces
+
+public static void main(String[] args) {
+
+	// data in java nees to have a type declared
+	int x = 5;
+	// java will turn this into 4.0
+	float y = 4;
+	double z = 5.2;
+
+	//booleans are either true or false
+	boolean val = true;
+	char myChar = 'A';
+	String name = "Mr.Considine";
+
+	//print the data
+	System.out.println(x);
+	System.out.println(y + z);
+	System.out.println(20 % 3);
+	System.out.println(x++);
+	System.out.println(x++);
+	System.out.println(x++);
+
+
+	//numerical data treats opperator with math
+	//string data treats operatiors with concatenation
+	System.out.println(val);
+	System.out.println(myChar);
+
+
+
+	// java operators
+	// +, -, *, /, %, ++, --
+	// modulo opeartor calculates the remainder
+
+}
+
+
+}
