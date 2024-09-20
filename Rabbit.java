b1d26065aa9730a2530bf06b3c851a52ae2559ed
Lisha (Lisa) Liu <lisha_liu@m018761.aw.org>
2024-09-20 13:53:05 -0700

    message

---
+++ Rabbit.java
@@ -0,0 +1,41 @@
+class Rabbit{
+
+String name ;
+int age;
+String color;
+double weight;
+String breed;
+
+public String getName(){
+
+	return name;
+}
+
+public int getAge(){
+
+	return age;
+}
+
+public String getColor(){
+	return color;
+}
+
+public double getWeight(){
+
+	return weight;
+}
+
+public String getBreed(){
+
+	return breed;
+}
+public Rabbit(String n, int a, String c, double w, String b){
+
+name = n;
+age = a;
+color = c;
+weight = w;
+breed = b;
+
+}
+}
